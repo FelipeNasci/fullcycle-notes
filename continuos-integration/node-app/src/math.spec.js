ad0d053e2039d0cb40c901bcb8fc61ec4bd60efb
Diego Nascimento <dfnascimento@live.com>
2023-04-07 16:18:49 -0300

    ci: add step ci

--- continuos-integration/node-app/src/math.spec.js
+++ continuos-integration/node-app/src/math.spec.js
@@ -3,7 +3,7 @@ const { sum, sub, square } = require("./math");
 const my_tests = [
   { name: "sum", expected: sum(2, 3), toBeIt: 5 },
   { name: "sub", expect: sub(2, 3), toBe: -1 },
-  { name: "square", expect: square(2), toBe: 5 },
+  { name: "square", expect: square(2), toBe: 4 },
 ];
 
 describe("math", () => {
